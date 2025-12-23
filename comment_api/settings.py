0d316bace1eea29699cd30ca63ef8d4ea1893c7b
txia <xiatianrui@outlook.com>
2025-12-22 23:51:17 -0800

    restore database settings to default

--- comment_api/settings.py
+++ comment_api/settings.py
@@ -80,8 +80,8 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "comments_db",
-        "USER": "pengxia",
-        "PASSWORD": "",
+        "USER": "your_username",  # Change to your PostgreSQL username
+        "PASSWORD": "",  # Add password if needed
         "HOST": "localhost",
         "PORT": "5432",
     }
